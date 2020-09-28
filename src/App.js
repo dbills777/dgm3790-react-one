1092e0387f874b1989a07014125ca57de7388d36
Derrick Bills <dbills777@gmail.com>
2020-09-28 15:05:37 -0600

    refactored fetch

--- src/App.js
+++ src/App.js
@@ -12,30 +12,25 @@ const App = (props) => {
   const [items, setItems] = useState([]);
   const [quotes, setQuotes] = useState([]);
   const [query, setQuery] = useState('');
-
+  const fetchItems = async () => {
+    const result = await axios(
+      `https://www.breakingbadapi.com/api/${resourceType}=${query}`
+    );
+    if (resourceType === 'characters?name') {
+      setItems(result.data);
+      console.log(result);
+    }
+    if (resourceType === 'quote/random?author') {
+      setQuotes(result.data);
+    }
+    if (resourceType === 'episodes') {
+      setResourceType('quote/random?author');
+    }
+  };
   useEffect(() => {
-    const fetchItems = async () => {
-      const result = await axios(
-        `https://www.breakingbadapi.com/api/${resourceType}=${query}`
-      );
-      if (resourceType === 'characters?name') {
-        setItems(result.data);
-        console.log(result);
-      }
-      if (resourceType === 'quote/random?author') {
-        setQuotes(result.data);
-      }
-      if (resourceType === 'episodes') {
-        setResourceType('quote/random?author');
-      }
-    };
     fetchItems();
   }, [query, resourceType]);
 
-  // useEffect(() => {
-  //   console.log('render')
-
-  // }, [resourceType])
   return (
     <>
       <MenuAppBar getQuery={(search) => setQuery(search)} />
