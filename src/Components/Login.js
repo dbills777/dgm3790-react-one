6ace7da6ac15858d6009f8b11f1b43ae138703c3
Derrick Bills <dbills777@gmail.com>
2020-10-10 19:19:14 -0600

    form work

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,67 @@
-import React from 'react';
-export const Login = () => {
+import React, { useState } from 'react';
+
+import './Login.css';
+
+const IngredientForm = React.memo((props) => {
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [email, setEmail]= useState('')
+  const [member, setMember] = useState('')
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setMember({ first_name: first_name, last_name: last_name, email: email });
+    console.log('submitted')
+  };
+  console.log(member)
   return (
-    <div className='login-container'>
-        <div className='container'>
-          <h1>Login/Sign Up stuff here</h1>
-        </div>
+    <div className='container'>
+      <p>Mailing List Form</p>
+      <section className='signup-form'>
+        <form onSubmit={submitHandler}>
+          <div className='form-control'>
+            <label htmlFor='title'>Firts Name</label>
+            <input
+              type='text'
+              id='firstName'
+              value={first_name}
+              onChange={(event) => {
+                setFirstName(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='title'>Lastname</label>
+            <input
+              type='text'
+              id='lastName'
+              value={last_name}
+              required
+              onChange={(event) => {
+                setLastName(event.target.value);
+              }}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='amount'>Email</label>
+            <input
+              type='email'
+              id='email'
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+            />
+          </div>
+          <div className='signup-button'>
+            <button className='btn' type='submit'>
+              Join
+            </button>
+          </div>
+        </form>
+      </section>
     </div>
   );
-};
-export default Login;
+});
+
+export default IngredientForm;
