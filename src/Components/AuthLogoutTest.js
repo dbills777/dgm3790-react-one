8786ff1d631df2679af0a804950ea44a97dd4a06
Derrick Bills <dbills777@gmail.com>
2020-11-25 21:39:21 -0700

    redirect test

--- src/Components/AuthLogoutTest.js
+++ src/Components/AuthLogoutTest.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
 
 const AuthLogoutTest = () => {
   const { logout } = useAuth0();
 
   return (
+    <NavLink to = '/login'>
     <Button color='default' variant = 'contained' onClick={() => logout()}>
       Logout
     </Button>
+
+    </NavLink>
   );
 };
 
