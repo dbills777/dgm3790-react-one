6e7e7ac1377ccfc698c0aa4439a8ddbeba687a8c
Derrick Bills <dbills777@gmail.com>
2020-11-26 20:07:13 -0700

    trailer viedeo

--- src/Components/PersonalCard.js
+++ src/Components/PersonalCard.js
@@ -8,6 +8,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { useAuth0 } from '@auth0/auth0-react';
+import ReactPlayer from 'react-player';
+
 
 
 const useStyles = makeStyles({
@@ -29,40 +31,43 @@ export default function ImgMediaCard() {
 
   return (
     isAuthenticated && (
-      <Card className={classes.root}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.profile}
-            component='img'
-            alt='Contemplative Reptile'
-            height='140'
-            width='140'
-            image={user.picture}
-            title='Contemplative Reptile'
-          />
-         
-          <CardContent>
-            <Typography gutterBottom variant='h5' component='h2'>
-              {user.name}
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              First Name: {user.given_name}
-            </Typography>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              Last Name: {user.family_name}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size='small' color='primary'>
-            {user.email}
-          </Button>
-          <Button size='small' color='primary'>
-            Learn More
-          </Button>
-        </CardActions>
-        {console.log(user)}
-      </Card>
+      <>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.profile}
+              component='img'
+              alt='Contemplative Reptile'
+              height='140'
+              width='140'
+              image={user.picture}
+              title='Contemplative Reptile'
+            />
+
+            <CardContent>
+              <Typography gutterBottom variant='h5' component='h2'>
+                {user.name}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                First Name: {user.given_name}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                Last Name: {user.family_name}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size='small' color='primary'>
+              {user.email}
+            </Button>
+            <Button size='small' color='primary'>
+              Learn More
+            </Button>
+          </CardActions>
+          {console.log(user)}
+        </Card>
+        <ReactPlayer url='https://www.youtube.com/watch?v=F1HNuAE9WdU' playing = {true}></ReactPlayer>
+      </>
     )
   );
 }
